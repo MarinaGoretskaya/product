f804d2eafcb8b44824c918c07908ab3298fdfb5b
MarinaGoretskaya <marisha.2307@gmail.com>
2018-12-27 20:31:07 +0300

    add prod.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdComponent } from './components/prod/prod.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdComponent
   ],
   imports: [
     BrowserModule,
