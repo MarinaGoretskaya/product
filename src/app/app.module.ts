b63fedc69311dae0623c86552e672c42e9737d0e
MarinaGoretskaya <marisha.2307@gmail.com>
2018-12-27 20:18:27 +0300

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { Product1Component } from './product1/product1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    Product1Component
   ],
   imports: [
     BrowserModule,
