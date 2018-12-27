ac699a27bee88eaace419bb3fbe4e2349f238864
MarinaGoretskaya <marisha.2307@gmail.com>
2018-12-27 20:24:13 +0300

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
 import { Product1Component } from './product1/product1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
     Product1Component
   ],
   imports: [
